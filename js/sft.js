9c573ce27f7a15f109c08d73f10993a549661c1c
timkeane <tkeane@doitt.nyc.gov>
2018-08-06 18:15:54 -0400

    list counts for screen readers

--- js/sft.js
+++ js/sft.js
@@ -1,12 +1,7 @@
-nyc.ol.FinderApp.prototype.resetList = function() {};
-nyc.ol.FinderApp.prototype.focusFacilities = function() {};
-nyc.ol.FinderApp.prototype.adjustTabs = function() {};
-nyc.ol.FinderApp.prototype.adjustTabs = function() {};
-
-var app = new nyc.ol.FinderApp({
+new nyc.ol.FinderApp({
   title: 'Sandy Funding Tracker',
   geoclientUrl: 'https://maps.nyc.gov/geoclient/v1/search.json?app_key=74DF5DB1D7320A9A2&app_id=nyc-lib-example',
-  facilityTabTitle: '',
+  facilityTabTitle: 'Project Count',
   facilityUrl: CARTO_URL + encodeURIComponent('select ST_AsText(g.the_geom_webmercator) wkt_geom, s.council_district, count(council_district) count from single_family_data s, council g where s.council_district = g.cd group by s.council_district, g.the_geom_webmercator'),
   facilityFormat: new nyc.ol.format.CartoSql(),
   facilityStyle: style,
@@ -30,13 +25,15 @@ var app = new nyc.ol.FinderApp({
   decorations: [decorations]
 });
 
+finderApp.pager.pageSize = 20;
+
 $(document).ready(function() {
   var collapsible = new nyc.Collapsible({
     target: $('<div id="group"></div>'),
     title: 'Group by',
-    content: app.filters.geoGroup.getContainer()
+    content: finderApp.filters.geoGroup.getContainer()
   })
-  app.filters.geoGroup.on('change', app.filters.filter, app.filters);
+  finderApp.filters.geoGroup.on('change', finderApp.filters.filter, finderApp.filters);
   $('#filters').prepend(collapsible.getContainer());
-  app.tabs.open('#filters');
+  finderApp.tabs.open('#filters');
 });
